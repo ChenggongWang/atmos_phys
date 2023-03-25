2d46ee945de1fb0c3c16dab14b0c4839827aae16
Chenggong Wang <c.wang@princeton.edu>
2023-03-24 23:36:38 -0400

    seperate NN module 1: initialization

--- atmos_param/radiation/driver/radiation_driver.F90
+++ atmos_param/radiation/driver/radiation_driver.F90
@@ -172,7 +172,8 @@ use radiation_driver_diag_mod, only: radiation_driver_diag_init, &
                                      produce_radiation_diagnostics, &
                                      write_solar_interp_restart_nc
 use radiation_driver_nn_mod,    only:   radiation_driver_nn_init, &
-                                        test
+                                        test, &
+                                        NN_FC_type
 !--------------------------------------------------------------------
 
 implicit none 
@@ -542,22 +543,6 @@ end type radiation_diag_type
 !     cldsasymm   cloud asymmetry factor  [ dimensionless ]
 
 
-!----------------------------------------------------------------------
-! cgw
-! define type for linear layer
-! contains weight(2d) and bias(1d)
-!----------------------------------------------------------------------
-public NN_Linear_layer_type
-type :: NN_Linear_layer_type 
-    real(kind=4), dimension(:,:), pointer :: weight=>NULL()
-    real(kind=4), dimension(:),   pointer :: bias=>NULL()
-end type NN_Linear_layer_type
-public NN_FC_type
-type :: NN_FC_type
-    integer :: num_hid_nodes
-    integer :: num_layers
-    type(NN_Linear_layer_type), dimension(:), pointer:: Layers
-end type NN_FC_type
 
 !---------------------------------------------------------------------
 !---- private data ----
@@ -909,11 +894,6 @@ type(radiation_flux_type),   intent(inout) :: Rad_flux(:)
       logical :: Rad_restart_exists, Til_restart_exists, Rad_restart_conc_exists, Til_restart_conc_exists
       integer, allocatable, dimension(:) :: pes !< Array of pes in the current pelist
       character(len=32) :: mod_name
-      character(len=32) :: fmt_str
-      type(FmsNetcdfFile_t)       :: Rad_NN_para_fileobj        !< Fms2_io fileobj
-      character(len=100), dimension(4) :: rad_nn_para_nc_4filepath
-      integer :: nn_size0, nn_size1, inn
-      integer :: nn_num_layers, ilayer
       integer, dimension(4) :: a
 !---------------------------------------------------------------------
 !   local variables
@@ -1532,55 +1512,54 @@ type(radiation_flux_type),   intent(inout) :: Rad_flux(:)
 !         initialize NN module 
 !         register variale to diagnose the results from NN
 !---------------------------------------------------------------------
-    !if (do_rad_nn) &
-    !    call radiation_driver_nn_init()
+    if (do_rad_nn) &
+        call radiation_driver_nn_init(rad_nn_para_nc, Rad_NN_FC)
     if (do_rad_nn) then
-        outunit = stdout()
         ! read para file  
-        call error_mesg ('radiation_driver_mod',  &
-                 'Initializing the rad_nn module', NOTE)
-        call error_mesg ('radiation_driver_mod',  &
-                 'Reading NetCDF file to obtain NN parameters. INPUT/'//trim(rad_nn_para_nc), NOTE)
-        rad_nn_para_nc_4filepath(1) = "INPUT/"//trim(rad_nn_para_nc)//"lw_csaf_Li5Relu_EY.nc"
-        rad_nn_para_nc_4filepath(2) = "INPUT/"//trim(rad_nn_para_nc)//"lw_af_Li5Relu_EY.nc"
-        rad_nn_para_nc_4filepath(3) = "INPUT/"//trim(rad_nn_para_nc)//"sw_csaf_Li5Relu_EY.nc"
-        rad_nn_para_nc_4filepath(4) = "INPUT/"//trim(rad_nn_para_nc)//"sw_af_Li5Relu_EY.nc"
-        
-        do inn = 1, 4
-            if (open_file(Rad_NN_para_fileobj, rad_nn_para_nc_4filepath(inn), "read" )) then
-                ! read num of NN layers
-                call read_data(Rad_NN_para_fileobj, 'LN', nn_num_layers)
+        !call error_mesg ('radiation_driver_mod',  &
+        !         'Initializing the rad_nn module', NOTE)
+        !call error_mesg ('radiation_driver_mod',  &
+        !         'Reading NetCDF file to obtain NN parameters. INPUT/'//trim(rad_nn_para_nc), NOTE)
+        !rad_nn_para_nc_4filepath(1) = "INPUT/"//trim(rad_nn_para_nc)//"lw_csaf_Li5Relu_EY.nc"
+        !rad_nn_para_nc_4filepath(2) = "INPUT/"//trim(rad_nn_para_nc)//"lw_af_Li5Relu_EY.nc"
+        !rad_nn_para_nc_4filepath(3) = "INPUT/"//trim(rad_nn_para_nc)//"sw_csaf_Li5Relu_EY.nc"
+        !rad_nn_para_nc_4filepath(4) = "INPUT/"//trim(rad_nn_para_nc)//"sw_af_Li5Relu_EY.nc"
+        !
+        !do inn = 1, 4
+        !    if (open_file(Rad_NN_para_fileobj, rad_nn_para_nc_4filepath(inn), "read" )) then
+        !        ! read num of NN layers
+        !        call read_data(Rad_NN_para_fileobj, 'LN', nn_num_layers)
 
-                Rad_NN_FC(inn)%num_layers = nn_num_layers
-                allocate(Rad_NN_FC(inn)%Layers(Rad_NN_FC(inn)%num_layers))
-                ! read weight and bias for each layer
-                do ilayer = 1, nn_num_layers 
-                    ! read size of each layer
-                    write (fmt_str, "(A4,I1,I1)") "size", ilayer, 0
-                    call read_data(Rad_NN_para_fileobj, fmt_str, nn_size0)
-                    write (fmt_str, "(A4,I1,I1)") "size", ilayer, 1
-                    call read_data(Rad_NN_para_fileobj, fmt_str, nn_size1)
-                    allocate(Rad_NN_FC(inn)%Layers(ilayer)%weight(nn_size1,nn_size0)) !fortran reverse order
-                    allocate(Rad_NN_FC(inn)%Layers(ilayer)%bias(nn_size0))
-                    write(outunit,*) 'nn_size', nn_size0, nn_size1
-                    write (fmt_str, "(A1,I1)") "W", ilayer
-                    call read_data(Rad_NN_para_fileobj, fmt_str, Rad_NN_FC(inn)%Layers(ilayer)%weight(:,:))
-                    write (fmt_str, "(A1,I1)") "B", ilayer
-                    call read_data(Rad_NN_para_fileobj, fmt_str, Rad_NN_FC(inn)%Layers(ilayer)%bias(:))
-                    ! for diagnose purpose
-                    write(outunit,*) Rad_NN_FC(inn)%Layers(ilayer)%bias 
-                end do 
-                Rad_NN_FC(inn)%num_hid_nodes = nn_size1
-                call close_file(Rad_NN_para_fileobj)
-            else
-                call error_mesg ('radiation_driver_mod',  &
-                     'rad_nn_para_nc file open failed. '//trim(rad_nn_para_nc_4filepath(inn)), FATAL)
-            endif
-        end do
+        !        Rad_NN_FC(inn)%num_layers = nn_num_layers
+        !        allocate(Rad_NN_FC(inn)%Layers(Rad_NN_FC(inn)%num_layers))
+        !        ! read weight and bias for each layer
+        !        do ilayer = 1, nn_num_layers 
+        !            ! read size of each layer
+        !            write (fmt_str, "(A4,I1,I1)") "size", ilayer, 0
+        !            call read_data(Rad_NN_para_fileobj, fmt_str, nn_size0)
+        !            write (fmt_str, "(A4,I1,I1)") "size", ilayer, 1
+        !            call read_data(Rad_NN_para_fileobj, fmt_str, nn_size1)
+        !            allocate(Rad_NN_FC(inn)%Layers(ilayer)%weight(nn_size1,nn_size0)) !fortran reverse order
+        !            allocate(Rad_NN_FC(inn)%Layers(ilayer)%bias(nn_size0))
+        !            write(outunit,*) 'nn_size', nn_size0, nn_size1
+        !            write (fmt_str, "(A1,I1)") "W", ilayer
+        !            call read_data(Rad_NN_para_fileobj, fmt_str, Rad_NN_FC(inn)%Layers(ilayer)%weight(:,:))
+        !            write (fmt_str, "(A1,I1)") "B", ilayer
+        !            call read_data(Rad_NN_para_fileobj, fmt_str, Rad_NN_FC(inn)%Layers(ilayer)%bias(:))
+        !            ! for diagnose purpose
+        !            write(outunit,*) Rad_NN_FC(inn)%Layers(ilayer)%bias 
+        !        end do 
+        !        Rad_NN_FC(inn)%num_hid_nodes = nn_size1
+        !        call close_file(Rad_NN_para_fileobj)
+        !    else
+        !        call error_mesg ('radiation_driver_mod',  &
+        !             'rad_nn_para_nc file open failed. '//trim(rad_nn_para_nc_4filepath(inn)), FATAL)
+        !    endif
+        !end do
 
         ! diag
         mod_name = "NN_diag"
-        write(outunit,*) 'register output mod: ', mod_name
+        !write(outunit,*) 'register output mod: ', mod_name
         a(1:2) = axes(1:2)
         ! pfull
         a(3) = axes(3)
@@ -1607,7 +1586,7 @@ type(radiation_flux_type),   intent(inout) :: Rad_flux(:)
     endif ! do_rad_nn
     ! diag
     mod_name = "NN_diag"
-    write(outunit,*) 'register output mod: ', mod_name
+    !write(outunit,*) 'register output mod: ', mod_name
     a(1:2) = axes(1:2)
     a(3) = axes(4)
     !3D atmosphere fields.
